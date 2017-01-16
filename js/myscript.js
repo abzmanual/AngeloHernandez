a3a09ea3dac261c7d84b48e782c57d4b15459b90
abzmanual <mygitacct@gmail.com>
2017-01-16 18:42:26 +0800

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,58 @@
 
+// extension:
+$.fn.scrollEnd = function(callback, timeout) {
+  $(this).scroll(function(){
+    var $this = $(this);
+    if ($this.data('scrollTimeout')) {
+      clearTimeout($this.data('scrollTimeout'));
+    }
+    $this.data('scrollTimeout', setTimeout(callback,timeout));
+  });
+};
 
-$(document).ready(function(){
-   $(window).bind('scroll', function() {
-      var scrollTop = $(window).scrollTop();
-      var elementOffset = $('works').offset().top;
-      var currentElementOffset = (elementOffset - scrollTop);
-	  $('.workoffset').innerHTML = currentElementOffset;
-   });
-});
+function removeAllActiveClass(){
+  $(".menu .nav .active").removeClass("active");
+}
+
+function getActiveSection(){
+  var homePos = $('#home').offset().top - $(window).scrollTop(),
+  aboutPos = $('#about').offset().top - $(window).scrollTop(),
+  worksPos = $('#works').offset().top - $(window).scrollTop(),
+  experiencePos = $('#experience').offset().top - $(window).scrollTop(),
+  skillsPos = $('#skills').offset().top - $(window).scrollTop(),
+  contactPos = $('#contact').offset().top - $(window).scrollTop();   
+
+  $('.worksPos').html(worksPos);
+  if(homePos < 100){
+  	removeAllActiveClass();
+  	$('li.home').addClass('active');
+  }
+  if(aboutPos < 100){  
+  	removeAllActiveClass();
+  	$('li.about').addClass('active');
+  }
+  if(worksPos < 100){  
+  	removeAllActiveClass();
+  	$('li.works').addClass('active');
+  }
+  if(experiencePos < 100){  
+  	removeAllActiveClass();
+  	$('li.experience').addClass('active');
+  }
+  if(skillsPos < 100){  
+  	removeAllActiveClass();
+  	$('li.skills').addClass('active');
+  }
+  if(contactPos < 100){  
+  	removeAllActiveClass();
+  	$('li.contact').addClass('active');
+  }  
+}
+
+getActiveSection();
+$(window).scrollEnd(function(){
+    getActiveSection();
+}, 80);
 
 
 $(function() {
@@ -15,8 +60,9 @@ $(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	  $(".menu .nav .active").removeClass('active');
-	  $(this).parent('li').addClass('active');
+//	  removeAllActiveClass();
+//	  $(this).parent('li').addClass('active');
+	  getActiveSection();
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top
